89d8de56f3972b3624990958a8de8cf4028c4edf
Dalmas <akumuodhiambo@gmail.com>
2023-08-07 09:09:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const url = "https://anapioficeandfire.com/api/books"
+  const fetcher = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(resData => {
+      renderBooks(resData)
+    })
+  }
+  return fetcher()
 }
 
 function renderBooks(books) {
